b403c2cc7390933b330cbe43d33cdf51f2bc0860
jackvijayy <jackvijay1108@gmail.com>
2025-02-25 22:55:49 +0530

    Home Layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+
+const Home=()=> {
+  return (
+    <>
+    <BookOverview/>
+
+    <BookList/>
+    
+    </>
+    
+  
+  );
+}
+
+export  default Home;
